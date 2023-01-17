f9e413ebaa819e61017bd5aee2cdcad1e20fb2d8
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-17 03:17:36 +0300

    Task to connect JWT Auth to client completed

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { App } from './App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { App } from './App';
 import { setupStore } from './store/store';
 
 const store = setupStore();
