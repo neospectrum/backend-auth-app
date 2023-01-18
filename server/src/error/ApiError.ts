0f8d7c2984acfd7766c320ab8bdccbc9578c7d55
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-18 18:49:03 +0300

    comments

--- server/src/error/ApiError.ts
+++ server/src/error/ApiError.ts
@@ -2,27 +2,33 @@ export class ApiError extends Error {
     status: number;
     errors: any;
 
+    // Creating Error
     constructor(status: number, message: string, errors: Array<any> = []) {
         super(message);
         this.errors = errors;
         this.status = status;
     }
-
+    // Bad Request 400
     static badRequest(message: string, errors: Array<any> = []) {
         return new ApiError(400, message, errors);
     }
+    // Unauth error 401
     static unauthorized(errors: Array<any> = []) {
         return new ApiError(401, 'User unauthorized');
     }
+    // Forbidden 403
     static forbidden(message: string, errors: Array<any> = []) {
         return new ApiError(403, message, errors);
     }
+    // Not Found 404
     static notFound(message: string, errors: Array<any> = []) {
         return new ApiError(404, message, errors);
     }
+    // Not Acceptable 406
     static notAcceptable(message: string, errors: Array<any> = []) {
         return new ApiError(406, message, errors);
     }
+    // Conflict 409
     static conflict(message: string, errors: Array<any> = []) {
         return new ApiError(409, message, errors);
     }
--- server/src/middlewares/errorMiddleware.ts
+++ server/src/middlewares/errorMiddleware.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from 'express';
 import { ApiError } from '../error/ApiError.js';
+import colors from 'colors';
 
 // Error handling middleware
 export const errorMiddleware = (err: any, req: Request, res: Response, next: Function) => {
-    console.log(err);
+    console.log(colors.red(err));
 
+    // If it instance of ApiError, sending ApiError
     if (err instanceof ApiError) {
         return res.status(err.status).json({ message: err.message, errors: err.errors });
     }
+
     return res.status(500).json({ message: 'Unhappend error' });
 };
