5d95dfa807d56a3102084fc61f8dff02a00369ac
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-24 07:34:54 +0300

    Refactored

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -4,6 +4,7 @@ import { model, Schema } from 'mongoose';
 const userSchema = new Schema({
     email: { type: String, unique: true, required: true },
     password: { type: String, required: true },
+    role: { type: String, default: 'USER' },
     isActivated: { type: Boolean, default: true },
     activationLink: { type: String },
 });
