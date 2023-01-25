95ddd924ba994965c55c29e33422c0c84200a3d2
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-25 07:58:04 +0300

    Edited

--- server/src/dtos/user.dto.ts
+++ server/src/dtos/user.dto.ts
@@ -3,10 +3,12 @@ export class UserDto {
     email: string;
     id: string;
     isActivated: boolean;
+    role: string;
 
     constructor(model: any) {
         this.email = model.email;
         this.id = model._id;
         this.isActivated = model.isActivated;
+        this.role = model.role;
     }
 }
