05ada7caa5944b5413bb3a8d187695918e611227
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-26 07:47:38 +0300

    added check role middleware

--- server/src/routes/device.routes.ts
+++ server/src/routes/device.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
+
+import { checkRoleMiddleware } from './../middlewares/checkRole.middleware.js';
 import { deviceController } from '../controllers/device.controller.js';
 
 export const deviceRouter = Router();
 
 // Creating, updating, deleting device
-deviceRouter.post('/', deviceController.create);
-deviceRouter.patch('/', deviceController.update);
-deviceRouter.delete('/', deviceController.delete);
+deviceRouter.post('/', checkRoleMiddleware, deviceController.create);
+deviceRouter.patch('/', checkRoleMiddleware, deviceController.update);
+deviceRouter.delete('/', checkRoleMiddleware, deviceController.delete);
 
 // Getting devices
 deviceRouter.get('/', deviceController.getAll);
