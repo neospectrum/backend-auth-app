10bd81edb3455eb397bd8a75b2ebc413d851cefb
neospectrum <105888081+neospectrum@users.noreply.github.com>
2023-01-28 22:27:05 +0300

    Configured device endpoint

--- client/src/services/device.ts
+++ client/src/services/device.ts
@@ -3,8 +3,31 @@ import { api } from './api';
 const deviceApi = api.injectEndpoints({
     endpoints: (build) => ({
         getOne: build.query<any, any>({
-            query: (id) => ({
+            query: (name) => ({
+                url: `/${name}`
+            })
+        }),
+        getAll: build.query<any, any>({
+            query: () => ({
                 url: '/'
+            })
+        }),
+        createDevice: build.mutation<any, any>({
+            query: () => ({
+                url: '/',
+                method: 'POST'
+            })
+        }),
+        updateDevice: build.mutation<any, any>({
+            query: () => ({
+                url: '/',
+                method: 'PATCH'
+            })
+        }),
+        deleteDevice: build.mutation<any, any>({
+            query: () => ({
+                url: '/',
+                method: 'DELETE'
             })
         })
     })
